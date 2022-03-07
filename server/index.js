6cd1a532e54a66f6c71a8e234577edb3976f050e
Peter hp <phirschparker@gmail.com>
2022-03-07 17:48:11 +0100

    style(index.js): linting single file

--- server/index.js
+++ server/index.js
@@ -1,10 +1,11 @@
-const express = require("express");
+const express = require('express');
+
 const app = express();
-const dotenv = require("dotenv");
+
 const PORT = process.env.PORT || 3001;
-const { router } = require("./router");
-const { startServer } = require("./db.js");
-const cors = require('cors')
+const cors = require('cors');
+const { router } = require('./router');
+const { startServer } = require('./db');
 
 app.use(cors());
 app.use(express.json());
@@ -12,8 +13,6 @@ app.use(router);
 
 startServer()
   .then(
-    app.listen(PORT, () =>
-      console.log(`Server listening at http://localhost:${PORT} 🤯`)
-    )
+    app.listen(PORT, () => console.log(`Server listening at http://localhost:${PORT} 🤯`)),
   )
   .catch((e) => console.error(e));
