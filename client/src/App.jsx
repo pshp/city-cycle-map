835269fb8a68d32d63e35e172d95b5da82d574ff
Peter hp <phirschparker@gmail.com>
2022-03-08 12:36:22 +0100

    refactor(n): deleted unused files

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 import { MapProvider } from 'react-map-gl';
 import BaseMap from './componants/BaseMap';
-import DirectionsMap from './componants/DirectionsMap';
+
 function App() {
   return (
     <div className="App">
@@ -11,7 +11,6 @@ function App() {
           <BaseMap />
         </MapProvider>
       </div>
-      {/* <DirectionsMap/> */}
     </div>
   );
 }
